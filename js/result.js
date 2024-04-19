4d37017fe5524ad61ee664d8a7b4df609874e72a
Barkamol <kamolbek0990@gmail.com>
2024-04-19 15:18:49 +0500

    added new section

---
+++ js/result.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll(".grid-item");
+cards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cards.forEach((el) => el.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
