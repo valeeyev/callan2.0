8fe04e8bd084ac48368367af8f217e1485cb99a1
Barkamol <kamolbek0990@gmail.com>
2024-04-21 13:40:17 +0500

    final commits

--- js/courses.js
+++ js/courses.js
@@ -11,3 +11,32 @@ var swiper = new Swiper(".blog-slider", {
     clickable: true,
   },
 });
+
+// !go link func
+$(document).ready(function () {
+  // Check if the URL contains a hash
+  if (window.location.hash) {
+    // Get the position of the section
+    var sectionPos = $(window.location.hash).offset().top;
+
+    // Subtract the height of the navbar and an additional 10 pixels from the position
+    var navbarHeight = $(".navigation").outerHeight();
+    sectionPos = sectionPos - navbarHeight - 30;
+
+    // Scroll to the corresponding section
+    $("html, body").animate(
+      {
+        scrollTop: sectionPos,
+      },
+      500
+    );
+
+    // Add a CSS class to highlight the section
+    $(window.location.hash).addClass("highlighted");
+
+    // Remove the highlight after some time
+    setTimeout(function () {
+      $(window.location.hash).removeClass("highlighted");
+    }, 3000); // 3000 milliseconds = 3 seconds
+  }
+});
